12a014cdb90d78d8f0427cc9ddf7281bf5fb64f9
Aldrich Wright <aldrichwright@hotmail.com>
2017-09-12 21:08:28 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forloop(arrayIn) {
+  for (let i = 0; i < 25; i++) {
+    // ...
+  if arrayIn[i] = 1 {
+    arrayIn.push( "I am 1 strange loop" );
+  }
+  else {
+    arrayIn.push(`"I am ${i} strange loops."`);
+  }
+
+}
